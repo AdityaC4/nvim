87563b54d17d852b432f885e3345ecc1cd39d0aa
AdityaC4 <adityapchaudhari@gmail.com>
2026-02-22 11:54:27 -0600

    add: aerial.lua

---
+++ lua/aditya/lazy/aerial.lua
@@ -0,0 +1,24 @@
+return {
+  'stevearc/aerial.nvim',
+  opts = {},
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("aerial").setup({
+      -- your preferred options here, e.g.:
+      attach_mode = "global",
+      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    })
+  end,
+  -- Set a keymap to toggle aerial
+  keys = {
+    { "<leader>A", "<cmd>AerialToggle!<CR>", desc = "Aerial: Toggle" }
+  },
+}
